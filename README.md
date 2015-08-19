ff70f0e81b7dd79cb2d77bbc0cde9cdc2aab8072
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2015-08-20 00:53:58 +0700

    import from wp-theme-subblog_v2

---
+++ README.md
@@ -0,0 +1,4 @@
+wp-theme-subblog_v2
+===================
+
+WordPress theme subblog v2
---
+++ content_bottom_ad.php
@@ -0,0 +1,11 @@
+			<?php if ( is_single() ) : ?>
+<div class="clear">
+    <div class="ad">
+        <?php echo get_coppi('ga_content_bottom1'); ?>
+    </div>
+    <div class="ad">
+        <?php echo get_coppi('ga_content_bottom2'); ?>
+    </div>
+			<?php endif; // is_single() ?>
+
+
